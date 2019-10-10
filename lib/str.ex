8ff49fce7a0a997eaba3d96cbfb2b3942e46c29b
Franco Catena <francocatena@gmail.com>
2019-10-10 14:47:12 -0300

    Use defdelegate on main module

--- lib/str.ex
+++ lib/str.ex
@@ -1,11 +1,6 @@
 defmodule Str do
   alias Str.Upload
 
-  def upload(bucket, file, attempt \\ 0) do
-    Upload.upload(bucket, file, attempt)
-  end
-
-  def handle_upload_response(response, bucket, file, attempt \\ 0) do
-    Upload.handle_upload_response(response, bucket, file, attempt)
-  end
+  defdelegate upload(bucket, file, attempt \\ 0), to: Upload
+  defdelegate handle_upload_response(response, bucket, file, attempt \\ 0), to: Upload
 end
